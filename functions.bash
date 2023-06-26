6ce9055bfbfd5613a55dde6fb62138990d485464
T. Hasid <tzipih@skyvar.co.il>
2023-06-26 12:52:18 +0300

    finish files

--- functions.bash
+++ functions.bash
@@ -2,21 +2,29 @@
 factorial()
 {
 
-    if [ $1 -gt $2 ]
-    then
-        rs=`expr $i % 2`
-        echo rs
-        if [ rs -eq 0 ]
-        then
-            echo $1
-            factorial $(($1 + 2)) $2  
-        fi
+    if [ $1 -gt $2 ]; then
+
+       return
+
+    fi
+
+    if [[ $(( $1 % 2 )) -eq 0 ]]; then
+        echo $1
+        factorial $(($1 + 2)) $2 
+
+    else    
+
         factorial $(($1 + 1)) $2 
+
     fi
  
 }
 
-factorial
+# factorial $1 $2
 
-
+main () {
+    echo $FUNCNAME
+    factorial $1 $2
+}
+main $1 $2
 
